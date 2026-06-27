a2543ca976fb94b67a327a6e47ae1bec6173442a
dandirayo <72259228+dandirayo@users.noreply.github.com>
2026-06-27 21:34:11 +0700

    Update

--- backend-dotnet/obj/Debug/net8.0/backend-dotnet.AssemblyInfo.cs
+++ backend-dotnet/obj/Debug/net8.0/backend-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08879456d1839c624fc5eb6fe1bf04740f7ebc4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acec8748711e77310776f6c300ec9984798092a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
